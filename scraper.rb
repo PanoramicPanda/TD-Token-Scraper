83b4dbc6e6fae79a754a77f209c4fa19e2af39d2
PanoramicPanda <uriah.h.brown@gmail.com>
2019-05-30 19:32:45 -0400

    delete old test file

--- scraper.rb
+++ scraper.rb
@@ -1,1 +1,12 @@
 require 'token'
+
+
+# mega_hash = {}
+# test_array.each_with_index do |token, index|
+#   token.name = token.name + index.to_s
+#   token.setup_token_image('http://tokendb.com/wp-content/uploads/2012/06/Gauntlets-of-Ogre-Power-100x100.jpg')
+#   token.download_token_image
+#   mega_hash[token.name] = token.token_hash
+# end
+#
+# File.write("tokens.json", JSON.pretty_generate(mega_hash))
