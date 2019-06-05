f3efd38deab6780bf95d0f8471fbfac7823ccf35
PanoramicPanda <uriah.h.brown@gmail.com>
2019-06-04 20:35:47 -0400

    Making the time reporting more robust

--- helpers/debugger.rb
+++ helpers/debugger.rb
@@ -14,7 +14,21 @@ class TDDebugger
   end
 
   def elapsed_time(task)
-    debug_message("#{task} took #{(Time.now - @start_time).to_i} seconds") if @debug
+    debug_message("#{task} took #{time_string(Time.now - @start_time).to_i}") if @debug
+  end
+
+  def time_string(total_time)
+    hours = total >= 3600 ? total / 3600 : 0
+    under_hour_seconds = hours == 0 ? total_time : total_time % 3600
+    minutes = under_hour_seconds >= 60 ? total_time / 60 : 0
+    seconds = total_time - (minutes * 60) - (hours * 3600)
+
+    composite = ''
+    composite += "#{hours} hours, " if hours != 0
+    composite += "#{minutes} minutes, " if minutes != 0
+    composite += "and " if hours != 0 || minutes != 0
+    composite += "#{seconds} seconds"
+    composite
   end
 
 end
